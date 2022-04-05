3ca4d48e471dfefa5ee53ee035a16fcf6766a546
Everton Carneiro <everton.mercante@gmail.com>
2022-04-05 14:18:32 -0300

    Created card layout

--- CustomUIKitNavigation/ContentView.swift
+++ CustomUIKitNavigation/ContentView.swift
@@ -7,15 +7,55 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct RootCheckoutView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                CheckoutCardView(description: "Root checkout screen", buttonImage: Image(systemName: "arrow.forward.circle")) {
+                    
+                }
+            }
+            .navigationTitle("Checkout")
+        }
+        
+    }
+}
+
+struct CheckoutCardView: View {
+    let description: String
+    let buttonImage: Image
+    let onTap: () -> Void
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text(description)
+                    .font(.system(size: 16, weight: .bold, design: .rounded))
+                Spacer()
+            }
+            
+            Spacer()
+            
+            HStack {
+                Spacer()
+                Button {
+                    onTap()
+                } label: {
+                    buttonImage
+                    .font(.system(size: 30, weight: .regular))
+                }
+            }
+        }
+        .padding()
+        .frame(width: 350, height: 150)
+        .background(Color(uiColor: .systemIndigo))
+        .foregroundColor(Color.white)
+        .cornerRadius(12)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        RootCheckoutView()
     }
 }
--- CustomUIKitNavigation/CustomUIKitNavigationApp.swift
+++ CustomUIKitNavigation/CustomUIKitNavigationApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct CustomUIKitNavigationApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            RootCheckoutView()
         }
     }
 }
