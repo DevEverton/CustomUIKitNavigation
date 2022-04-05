9a9b8c83ffbdf807720124abcedbf84f2422cace
Everton Carneiro <everton.mercante@gmail.com>
2022-04-05 10:48:49 -0300

    Initial Commit

---
+++ CustomUIKitNavigation/CustomUIKitNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomUIKitNavigationApp.swift
+//  CustomUIKitNavigation
+//
+//  Created by Everton Carneiro on 05/04/22.
+//
+
+import SwiftUI
+
+@main
+struct CustomUIKitNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
